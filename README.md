6e249b8d16f157b064bb8efb8a7b222402e961a8
Omkar Acharekar <omkaracharekar12@gmail.com>
2022-08-19 14:08:28 +0530

    Verified using Textcase

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
 # Serialize_and_Deserialize_DataStructure
 Implemented  the concept of Serializing and Deserializing having IEB128 encoding and decoding.
 
+# Serialized Format
+
+1 x LEB (1-x) bytes                Int32
+1 x LEB (1-x) bytes                Int64
+sizeof(float) bytes                Float
+sizeof(double)bytes                Double
+1 x LEB (1-x) bytes                U8Array.size()
+U8Array.size() x LEB (0-x) bytes   The `uint8_t`s in the vector
+
 
 ### Tasks
 
 - [x]  Basic Outline.
 - [x] LEB128 Encoding and Decoding Algorithms.
-- [ ] Decribe the implementation and Usage in Readme
+- [ ] Decribe the Implementation and Usage in Readme
 - [ ] Test Cases
--- main.cpp
+++ main.cpp
@@ -1,23 +1,21 @@
 #include "leb128.cpp"
-#include <fstream>
 #include <cstring>
-
+#include <fstream>
+#include <sstream>
 class Data
 {
+
+public:
     int32_t Int32;
-    int64_t Int64;
     float Float;
-    double Double;
     std::vector<uint8_t> U8Array;
 
     // serialize function : similar to std::vector<uint8_t> serialize(Data &D):
     friend std::ostream &operator<<(std::ostream &os, const Data &D)
     {
         os << LEB128(D.Int32);
-        os << LEB128(D.Int64);
-        // note that the float and double is in platform dependent binary format here:
+        // the float is in platform dependent binary format here:
         os.write(reinterpret_cast<const char *>(&D.Float), sizeof D.Float);
-        os.write(reinterpret_cast<const char *>(&D.Double), sizeof D.Double);
         os << LEB128(D.U8Array.size());
         for (auto val : D.U8Array)
             os << LEB128(val);
@@ -27,15 +25,14 @@ class Data
     // deserialize function  : similar to Data deserialize(std::vector<uint8_t> &Bytes);
     friend std::istream &operator>>(std::istream &is, Data &l)
     {
-        LEB128 lebInt32, lebInt64, lebSize;
-        char buf[sizeof l.Float];   // used to deserialize the float
-        char bufd[sizeof l.Double]; // used to deserialize the double
+        LEB128 lebInt32, lebSize;
+        char buf[sizeof l.Float];
 
-        if ((is >> lebInt32).read(buf, sizeof buf) && (is >> lebInt64).read(buf, sizeof buf) &&
-            (std::memcpy(&l.Float, buf, sizeof buf), is >> lebSize) && (std::memcpy(&l.Double, bufd, sizeof bufd), is >> lebSize))
+        // the float is in platform dependent binary format here:
+        if ((is >> lebInt32).read(buf, sizeof buf) &&
+            (std::memcpy(&l.Float, buf, sizeof buf), is >> lebSize))
         {
             l.Int32 = lebInt32.to<int32_t>();
-            l.Int64 = lebInt64.to<int64_t>();
             size_t size = lebSize.to<size_t>();
             l.U8Array.resize(size);
             LEB128 tmp;
@@ -49,10 +46,28 @@ class Data
     }
 };
 
+bool check(Data x, Data y)
+{
+    return x.Int32 == y.Int32 && x.Float == y.Float && x.U8Array == y.U8Array;
+}
+
 int main()
 {
+    Data h1{212538, 2.2743, {1, 1, 43, 14, 65, 26, 77, 98, 29, 6, 6, 21}};
+    std::stringstream ss;
+    ss << h1;
 
-    Data h1;
-    std::ofstream file("Data.bin");
-    file << h1;
+    Data h2;
+    ss >> h2;
+
+    // If h1 is equal to h2 then implementation is correct
+    std::cout << std::boolalpha << "Is h1 and h2 equal? " << check(h1, h2) << '\n';
+
+    // Data h1;
+    // std::ofstream file("Data.bin");
+    // file << h1;
+
+    // Data h2;
+    // std::ifstream file("Hello.bin");
+    // file >> h2;
 }
